57a7fe429b65c3664e0e0eca6585d040b547c145
yashg125 <yashg1357908@gmail.com>
2025-01-22 15:26:19 +0530

    Update progm.py

--- progm.py
+++ progm.py
@@ -1,29 +1,29 @@
-import numpy as ny
+import numpy as np
 #Q1
 sales =  [100, 200, 150, 300, 250, 400, 350]
-sales_array = ny.array(sales)
+sales_array = np.array(sales)
 print(sales_array)
 
 #Q2
-store_1 = ny.array([10, 20, 30])
-store_2 = ny.array([1, 2, 3])
-addition = store_1 + store_2
-multiplication = store_1 * store_2
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([1, 2, 3])
+addition = arr1 + arr_2
+multiply = arr1 * arr_2
 print("Addition result:", addition)
-print("Multiplication result:", multiplication)
+print("Multiplication result:", multiply)
 
 #Q3
-sales_january = ny.array([300, 400])
-sales_february = ny.array([500, 600])
-combined = ny.concatenate((sales_january, sales_february))
+sales_jan = np.array([300, 400])
+sales_feb = np.array([500, 600])
+combined = np.concatenate((sales_jan, sales_feb))
 print("Combined sales data:", combined)
 
 #Q4
-data = ny.array([1, 2, 3, 4, 5, 6])
+data = np.array([1, 2, 3, 4, 5, 6])
 reshaped = data.reshape(2, 3)
 print("Reshaped data:\n", reshaped)
 
 #Q5
-scores = ny.array([[80, 90], [85, 95]])
+scores = np.array([[80, 90], [85, 95]])
 flattened = scores.reshape(-1)
 print("Flattened scores:", flattened)
